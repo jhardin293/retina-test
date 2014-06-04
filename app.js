809731aab1bd75b267e58c047a59c4787e2b2200
justin hardin <jpmitchell293@gmail.com>
2014-06-04 12:20:20 -0700

    Added img

--- app.js
+++ app.js
@@ -11,15 +11,8 @@ function compile(str, path) {
     .set('filename', path)
     .use(nib())
 }
-//app.set('views', __dirname + '/views')
-//app.set('view engine', 'jade')
-
 app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
